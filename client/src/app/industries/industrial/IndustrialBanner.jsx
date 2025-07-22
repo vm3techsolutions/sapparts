f51d9937b92e3e53f5d381753d4fdb196097b406
Vishal Bhange <vishalbhange333@gmail.com>
2025-07-22 13:21:00 +0530

    Add Industries

--- client/src/app/industries/industrial/IndustrialBanner.jsx
+++ client/src/app/industries/industrial/IndustrialBanner.jsx
@@ -6,7 +6,7 @@ export default function MachineImage() {
     <div className="w-full bg-white flex justify-center items-center sm:px-4 mt-8">
       <div className="">
         <Image
-          src="/assets/industries/AgricultureBanner2.jpg" 
+          src="/assets/industries/IndustrialBanner.png" 
           alt="CNC Machining"
           width={933}
           height={512}
