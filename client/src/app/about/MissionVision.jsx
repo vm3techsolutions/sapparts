52e115c6346b9a093f1fa5e3a0f91759d9885194
Vishal Bhange <vishalbhange333@gmail.com>
2025-10-30 17:51:46 +0530

    New Changes updated

--- client/src/app/about/MissionVision.jsx
+++ client/src/app/about/MissionVision.jsx
@@ -3,29 +3,32 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 export default function MissionVisionSection() {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
+
   return (
     <div className="Section bg-[#F6F8FC]">
-      <h2 className="Heading text-center ">
+      <h2 className="Heading text-center">
         {t("Mission & Vision")}
       </h2>
 
-      <div className="max-w-2xl mx-auto flex flex-col md:flex-row justify-center items-stretch gap-12 mt-5">
-        {/* Vision Card */}
-        <div className="flex-1 bg-[#134285] text-white p-6 rounded-md text-center flex flex-col h-full min-h-[350px]">
+      <div className="max-w-2xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 mt-5">
+        
+        {/* Vision */}
+        <div className="bg-[#134285] text-white p-6 rounded-md text-center flex flex-col justify-start h-[350px] md:h-[350px] lg:h-[350px]">
           <h3 className="text-3xl font-semibold mb-4">{t("Vision")}</h3>
-          <p className="MissionVision text-center">
-          {t("Vision-Statement")}
+          <p className="MissionVision text-center overflow-hidden">
+            {t("Vision-Statement")}
           </p>
         </div>
 
-        {/* Mission Card */}
-        <div className="flex-1 bg-[#134285] text-white p-6 rounded-md text-center flex flex-col h-full min-h-[350px]">
+        {/* Mission */}
+        <div className="bg-[#134285] text-white p-6 rounded-md text-center flex flex-col justify-start h-[350px] md:h-[350px] lg:h-[350px]">
           <h3 className="text-3xl font-semibold mb-4">{t("Mision")}</h3>
-          <p className="MissionVision text-center">
-          {t("Mision-Statement")}
+          <p className="MissionVision text-center overflow-hidden">
+            {t("Mision-Statement")}
           </p>
         </div>
+
       </div>
     </div>
   );
